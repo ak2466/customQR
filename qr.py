f993b8a95821234ac050143a3fcb7c0c8ebd52fd
Alex King <ak2466@nau.edu>
2025-04-26 21:53:01 -0700

    Got rid of old text-based code for clarity

--- qr.py
+++ qr.py
@@ -1,27 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
 
-#print("".join("██" if cell else "  " for cell in row))
-#print("".join("HOLE" if cell else "  " for cell in row))
-#print("".join("H" if cell else "  " for cell in row))
-#print("".join("◻️" if cell else "◾️" for cell in row))
-#print("".join("██" if cell else "  " for cell in row))
-
-'''even = False
-for row in ascii_qr:
-    if even:
-        print("".join("HO" if cell else "  " for cell in row))
-    else:
-        print("".join("LE" if cell else "  " for cell in row))
-
-    even = not even
-    '''
-
-'''
-for index in range(0, len(ascii_qr)):
-    print("".join("HOLE" if cell else "    " for cell in ascii_qr[index]))
-    print("".join("LEHO" if cell else "    " for cell in ascii_qr[index]))
-    '''
-
 import qrcode
 
 qr = qrcode.QRCode(border=1)
@@ -30,8 +8,8 @@ qr.make()
 
 ascii_qr = qr.get_matrix()
 
-CELL_HEIGHT = 40
-CELL_WIDTH = 40
+CELL_HEIGHT = 20
+CELL_WIDTH = 20
 
 GRID_WIDTH = len(ascii_qr[0])
 GRID_HEIGHT = len(ascii_qr)
@@ -54,7 +32,7 @@ for y in range(GRID_HEIGHT):
     for x in range(GRID_WIDTH):
         value = ascii_qr[y][x]
         
-        text_top = "H" if value else "    "
+        text_top = "%" if value else "    "
         text_bottom = "H" if value else "    "
 
         xpos = x * CELL_WIDTH
